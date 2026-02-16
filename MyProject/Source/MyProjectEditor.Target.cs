2654f3e77e6c2b2ab322988d57b9d612df799e2b
pnnh <linyangz@outlook.com>
2026-02-16 16:43:53 +0800

    升级项目UE版本

--- MyProject/Source/MyProjectEditor.Target.cs
+++ MyProject/Source/MyProjectEditor.Target.cs
@@ -8,7 +8,7 @@ public class MyProjectEditorTarget : TargetRules
 	public MyProjectEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "MyProject" } );
 	}
